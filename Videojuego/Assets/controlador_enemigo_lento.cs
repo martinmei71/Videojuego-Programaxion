769ee8352a75afc32c8cc243d621d2707789fd2f
AnaBR1976 <76100380+AnaBR1976@users.noreply.github.com>
2021-02-07 23:04:06 +0100

    Creación Scrip controlador enemigo
    Movimiento hacia la izquierda

--- Videojuego/Assets/controlador_enemigo_lento.cs
+++ Videojuego/Assets/controlador_enemigo_lento.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class controlador_enemigo_lento : MonoBehaviour
 {
+    public float speed = -2f;
+    private Rigidbody2D enemigo;
+    private Animator animator;
+    
     // Start is called before the first frame update
     void Start()
+
     {
-        
+        enemigo = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemigo.velocity = new Vector2(speed, enemigo.velocity.y);
+       
     }
 }
