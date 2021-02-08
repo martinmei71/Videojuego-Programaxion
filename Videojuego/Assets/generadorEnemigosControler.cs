cdae497e7bbf65038385e79d2174a42258790a1b
AnaBR1976 <76100380+AnaBR1976@users.noreply.github.com>
2021-02-08 21:32:42 +0100

    Generador enemigos
    Generar transiciones, de animaciones .
    Prefab enemigo lengo creado, generador de enemigos lentos y creación de enemigo rápido

---
+++ Videojuego/Assets/generadorEnemigosControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generadorEnemigosControler : MonoBehaviour
+{
+
+    public GameObject prefabEnemigoLento;
+    public float intervalo = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("generarEnemigo", 0f, intervalo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void generarEnemigo()
+    {
+        Instantiate(prefabEnemigoLento, transform.position, Quaternion.identity);
+    }
+}
