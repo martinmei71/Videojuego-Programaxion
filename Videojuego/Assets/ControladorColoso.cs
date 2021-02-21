4f88f6207962ede0ee13fbe0775a8c329d320319
AnaBR1976 <76100380+AnaBR1976@users.noreply.github.com>
2021-02-21 17:55:43 +0100

    Creacion rangos

---
+++ Videojuego/Assets/ControladorColoso.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorColoso : MonoBehaviour
+{
+
+    public float speed;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Videojuego/Assets/rango_movimiento.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rango_movimiento : MonoBehaviour
+
+
+{
+    public Transform inicio;
+    public Transform fin;
+
+    private void OnDrawGizmosSelected()
+    {
+        if(inicio !=null && fin != null) // solo si tiene las variables valor
+        {
+            Gizmos.color = Color.blue; //selecionamos color azul para dibujar
+            Gizmos.DrawLine(inicio.position, fin.position); //dibuja una linea, los parametrso son inicio desde coloso hasta donde queremos que vaya
+            Gizmos.DrawWireSphere(fin.position, 0.1f); // dibuja un punto en la ´´ultima posición a la que llega
+
+        }
+    }
+
+   
+}
