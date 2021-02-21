d1bf5bf2a456dc9ecdb362386868b2c193d06818
AnaBR1976 <76100380+AnaBR1976@users.noreply.github.com>
2021-02-21 19:26:27 +0100

    Generar Movimiento Coloso

---
+++ Videojuego/Assets/RangoMovimiento.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangoMovimiento : MonoBehaviour
+{
+    public Transform inicio;
+    public Transform fin;
+
+    private void OnDrawGizmosSelected()
+    {
+        if(inicio !=null && fin != null)
+        {
+            Gizmos.color = Color.blue; // selecionamos color azul para dibujar.
+            Gizmos.DrawLine(inicio.position, fin.position);  //dibuja una linea y le damos los parametros de incio y de fin
+            Gizmos.DrawWireSphere(fin.position, 0.1f); // dibuja un punto en la última posición a la que llega
+        }
+    }
+}
